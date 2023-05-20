873abc4edf0969f95e67e5fa8a678a65d8f7b63b
Lucciano Campassi <106495109+lucciano19@users.noreply.github.com>
2023-05-20 11:12:12 -0300

    Add files via upload

---
+++ practica_27.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    cin >> i;
+
+    // devuelve el espacio de memoria
+    int numeros[i];
+    cout << sizeof(numeros) << " Bytes";
+
+    return 0;
+}
