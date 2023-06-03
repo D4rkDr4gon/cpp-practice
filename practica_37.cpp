71f08df9028c5fab6502c73439930f78338fb0e8
Lucciano Campassi <106495109+lucciano19@users.noreply.github.com>
2023-06-03 09:35:45 -0300

    Add files via upload

---
+++ practica_37.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //premite el ingreso de mas de una palabra a un string
+    string texto;
+
+    getline(cin,texto);
+
+    cout << texto;
+
+    return 0;
+}
