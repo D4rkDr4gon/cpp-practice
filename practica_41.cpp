af42392915cee7899b61c7b18b31aadb60d1f23d
Lucciano Campassi <106495109+lucciano19@users.noreply.github.com>
2023-07-01 09:07:31 -0300

    aa

--- practica_41.cpp
+++ practica_41.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-int main() {
-    
+struct Estudiante {
+int codigo;
+char nombre[50];
+int edad;
+};
 
+Estudiante dameEstudiante(int codigo) {
+    Estudiante estudiante;
+    estudiante.codigo = codigo;
+    cout << "Ingrese el nombre del estudiante: ";
+    cin.getline(estudiante.nombre, sizeof(estudiante.nombre));
+    cout << "Ingrese la edad del estudiante: ";
+    cin >> estudiante.edad;
+    cin.ignore(); // Ignorar el salto de línea pendiente en el búfer
+    return estudiante;
+}
+
+int main() {
+    Estudiante estudiantes[3];
+    
+    for (int i = 0; i < 3; i++) {
+        cout << "Ingrese los datos del estudiante " << i + 1 << ":" << endl;
+        estudiantes[i] = dameEstudiante(i + 1);
+    }
+    
+    FILE* archivo = fopen("estudiantes_binarios.dat", "wb");
+    
+    if (archivo != NULL) {
+        fwrite(estudiantes, sizeof(Estudiante), 3, archivo);
+        fclose(archivo);
+        cout << "Archivo creado exitosamente." << endl;
+    } else {
+        cout << "No se pudo crear el archivo." << endl;
+    }
     return 0;
 }
