388d68c1b7b7818f75b061066180cbaee0dc518c
Lucciano Campassi <106495109+lucciano19@users.noreply.github.com>
2023-09-09 11:02:33 -0300

    punteros

--- practica_46.cpp
+++ practica_46.cpp
@@ -1,7 +1,21 @@
-#include <iostream>
+#include <iostream> 
+#include <string> 
 using namespace std;
-
-int main(){
-
-    return 0;
-}
+ 
+// Definición del struct Alumno 
+struct Alumno { 
+    string nombre; 
+    int edad; 
+}; 
+ 
+int main() { 
+    // Declaración de un puntero a un Alumno 
+    Alumno* alumnoDinamico; 
+ 
+    // Asignación dinámica de memoria para un Alumno 
+    alumnoDinamico = new Alumno; 
+ 
+    // Asignar valores al Alumno dinámico 
+    alumnoDinamico->nombre = "Juan"; 
+    alumnoDinamico->edad = 20; 
+ 
