779d6d0bf4c916877a268f2bf91dff571d523772
Lucciano Campassi <106495109+lucciano19@users.noreply.github.com>
2023-09-09 11:03:17 -0300

    punteros

--- practica_46.cpp
+++ practica_46.cpp
@@ -19,3 +19,50 @@ int main() {
     alumnoDinamico->nombre = "Juan"; 
     alumnoDinamico->edad = 20; 
  
+// Imprimir los valores del Alumno dinámico 
+    std::cout << "Nombre del Alumno: " << alumnoDinamico->nombre << std::endl; 
+    std::cout << "Edad del Alumno: " << alumnoDinamico->edad << " años" << std::endl; 
+ 
+    // Liberar la memoria asignada dinámicamente 
+    delete alumnoDinamico; 
+ 
+    return 0; 
+}
+Operadores de Dirección y Desreferenciación
+El operador  &  se utiliza para obtener la dirección de memoria de una variable, mientras que el operador 
+*  se  utiliza para acceder al valor almacenado en una dirección de memoria.
+#include <iostream> 
+#include <string> 
+ 
+// Definición del struct Alumno 
+struct Alumno { 
+    std::string nombre; 
+    int edad; 
+}; 
+ 
+int main() { 
+    // Declaración de un objeto Alumno y un puntero 
+    Alumno juan; 
+    juan.nombre = "Juan"; 
+    juan.edad = 20; 
+ 
+    Alumno* punteroJuan = &juan; 
+ 
+    // Imprimimos la dirección de memoria y los valores almacenados en el objeto Alumno 
+    std::cout << "Dirección de memoria del objeto Alumno 'juan': " << punteroJuan << std::endl; 
+    std::cout << "Nombre del Alumno: " << punteroJuan->nombre << std::endl; 
+    std::cout << "Edad del Alumno: " << punteroJuan->edad << " años" << std::endl; 
+     
+    delete punteroJuan; 
+ 
+    return 0; 
+}
+Jugando con punteros
+#include <iostream> 
+using namespace std; 
+ 
+// Definición del struct Alumno 
+struct Alumno { 
+    string nombre; 
+    int edad; 
+}; 
