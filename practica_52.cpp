a98b58ae47b20912a8cb39e60efc2a5c3165c253
Lucciano Campassi <106495109+lucciano19@users.noreply.github.com>
2023-09-23 16:27:01 -0300

    spotify

--- practica_52.cpp
+++ practica_52.cpp
@@ -3,29 +3,29 @@
 using namespace std;
 
 struct Cancion {
-    sting nombre;
+    string nombre;
     int puntuacion = 0;
 };
 
 struct NodoCanciones {
-    Cancion nombre;
-    NodoCanciones* siguiente;
+    Cancion cancion;
+    NodoCanciones* siguiente = nullptr;
 };
 
 // Función para crear un nuevo nodo 
 NodoCanciones* crearNodo(string valor) { 
     NodoCanciones* nuevoNodo = new NodoCanciones; 
-    nuevoNodo->nombre.nombre = valor; 
-    nuevoNodo->siguiente = nullptr; 
+    nuevoNodo->cancion.nombre = valor; 
     return nuevoNodo; 
 } 
 
-Nodo* CargarHistorial(NodoCanciones* inicio, Cancion valor) { 
-    NodoCanciones* nuevoNodo = crearNodo(valor); 
+NodoCanciones* CargarHistorial(NodoCanciones* inicio, Cancion valor) { 
+    NodoCanciones* nuevoNodo = crearNodo(valor.nombre); 
+    nuevoNodo->cancion.puntuacion = valor.puntuacion; 
     if (inicio == nullptr) { 
         inicio = nuevoNodo; 
     } else { 
-        Nodo* actual = inicio; 
+        NodoCanciones* actual = inicio; 
         while (actual->siguiente != nullptr) { 
             actual = actual->siguiente; 
         } 
@@ -38,13 +38,12 @@ Nodo* CargarHistorial(NodoCanciones* inicio, Cancion valor) {
 void ImprimirHistorial(NodoCanciones* inicio) { 
     NodoCanciones* actual = inicio; 
     while (actual != nullptr) { 
-        cout << actual -> nombre.nombre << " " << actual -> nombre.puntuacion << endl; 
+        cout << actual->cancion.nombre << " " << actual->cancion.puntuacion << endl; 
         actual = actual->siguiente; 
     } 
-     
 }
- 
- // Función para insertar un elemento en la pila 
+
+// Función para insertar un elemento en la pila 
 void push(NodoCanciones*& pila, string valor) { 
     NodoCanciones* nuevoNodo = crearNodo(valor); 
     nuevoNodo->siguiente = pila; 
@@ -55,9 +54,9 @@ void push(NodoCanciones*& pila, string valor) {
 string pop(NodoCanciones*& pila) { 
     if (pila == nullptr) { 
         cout << "La playlist está vacía." << endl; 
-        return nullptr; // Valor de error 
+        return ""; // Valor de error 
     } 
-    string valor = pila->nombre; 
+    string valor = pila->cancion.nombre; 
     NodoCanciones* temp = pila; 
     pila = pila->siguiente; 
     delete temp; 
@@ -69,91 +68,88 @@ bool isEmpty(NodoCanciones* pila) {
     return pila == nullptr; 
 }
 
-//funcion para reproducir la primera cancion de la lista
-void Reproducir (NodoCanciones*& pila, NodoCanciones*& historial) {
-    Cancion proximaCancion;
-    
-    proximaCancion = pop(pila);
-    cout << "la cancion q se reproduce es: " << proximaCancion << endl;
-    cout << "puntuar la cancion: (1* - 5*)" << endl;
-    cin >> proximaCancion.puntuacion;
-    CargarHistorial(historial, proximaCancion);
-}
-
-//funcion q muestra la siguiente cancion
-void Mostrar (NodoCanciones*& pila) {
-    string proximaCancion;
-
-    proximaCancion = pila -> nombre;
-    cout << "la proxima cancion es: " << proximaCancion << endl;
-    
-}
-
-//funcion para invertir la playlist
-void Inversor(NodoCanciones*& pila) {
-    //creo una pila temporal
-    NodoCanciones* pilaAux = nullptr;
-    //la igualo a la pila
-    pilaAux = pila;
-
-    //vaciar pila
-    pila = nullptr;
-
-    //pasamos los valores de la pila auxiliar a la pila
-    while(!isEmpty(pilaAux)) {
-        string valor = pop(pilaAux);
-        push(pila, valor);
+// Función para reproducir la primera canción de la lista
+void Reproducir(NodoCanciones*& pila, NodoCanciones*& historial) {
+    if (isEmpty(pila)) {
+        cout << "La playlist está vacía." << endl;
+        return;
     }
 
+    string proximaCancion = pop(pila);
+    cout << "La canción que se reproduce es: " << proximaCancion << endl;
+    int puntuacion;
+    cout << "Puntuar la canción (1* - 5*): ";
+    cin >> puntuacion;
+
+    // Verificar que la puntuación esté en el rango válido (1-5)
+    if (puntuacion >= 1 && puntuacion <= 5) {
+        historial = CargarHistorial(historial, {proximaCancion, puntuacion});
+    } else {
+        cout << "Puntuación no válida. La canción no será puntuada." << endl;
+    }
 }
 
-int main(){
-    //inicializar la 
+// Función que muestra la siguiente canción
+void Mostrar(NodoCanciones* pila) {
+    if (isEmpty(pila)) {
+        cout << "La playlist está vacía." << endl;
+        return;
+    }
+
+    cout << "La próxima canción es: " << pila->cancion.nombre << endl;
+}
+
+// Función para invertir la playlist
+void Inversor(NodoCanciones*& pila) {
+    NodoCanciones* pilaAux = nullptr;
+    while (!isEmpty(pila)) {
+        string valor = pop(pila);
+        push(pilaAux, valor);
+    }
+    pila = pilaAux;
+}
+
+int main() {
     NodoCanciones* pila = nullptr;
     NodoCanciones* listaHistorial = nullptr;
-    
 
-    //bucle de la interfaz
-    while(true) {
+    while (true) {
         int opcion;
-        
         cout << "-------Spotify----------" << endl;
-        cout << "tu playlist:" << endl;
+        cout << "Tu playlist:" << endl;
         cout << "1. Agregar Cancion" << endl;
-        cout << "2. eliminar la primera cancion" << endl;
-        cout << "3. tomar cancion para reproducir" << endl;
-        cout << "4. invertir la playlist" << endl;
-        cout << "5. ver historial de reproduccion" << endl;
-        cout << "6. salir" << endl;
-        cout << "que desea hacer? ";
+        cout << "2. Eliminar la primera cancion" << endl;
+        cout << "3. Tomar cancion para reproducir" << endl;
+        cout << "4. Invertir la playlist" << endl;
+        cout << "5. Ver historial de reproduccion" << endl;
+        cout << "6. Salir" << endl;
+        cout << "¿Qué desea hacer? ";
         cin >> opcion;
+        cin.ignore();
 
         if (opcion == 1) {
             string cancion;
-            cout << "-----agregar cancion-------" << endl;
-            cout << "nombre de la cancion: ";
+            cout << "-----Agregar canción-------" << endl;
+            cout << "Nombre de la cancion: ";
             getline(cin, cancion);
-            //agrega a la pila
             push(pila, cancion);
-            cout << cancion << "añadida con exito" << endl;
+            cout << cancion << " añadida con exito." << endl;
         } else if (opcion == 2) {
             pop(pila);
-            cout << " primera cancion eliminada" << endl;
+            cout << "Primera cancion eliminada." << endl;
         } else if (opcion == 3) {
-            cout << "------panel de reproduccion-------" << endl;
-            Reproducir(pila);
-            Mostrar(pila);
+            cout << "------Panel de reproduccion-------" << endl;
+            Reproducir(pila, listaHistorial);
         } else if (opcion == 4) {
             Inversor(pila);
-            cout << "pila invertida" << endl;
-        } else if(opcion == 5) {
-                ImprimirHistorial(historial);
-        }
-         else if (opcion == 6) {
-            cout << "reproduccion terminada" << endl;
+            cout << "Playlist invertida." << endl;
+        } else if (opcion == 5) {
+            ImprimirHistorial(listaHistorial);
+        } else if (opcion == 6) {
+            cout << "Reproduccion terminada." << endl;
             break;
         } else {
-            cout << "opcion invalida" << endl;
+            cout << "Opcion invalida." << endl;
         }
     }
 
